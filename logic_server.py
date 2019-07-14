4e7f0a80d4eff95bcc8c789493b92663b8120a72
Ihor Sochyvets <isochivets@gmail.com>
2019-07-14 20:44:29 +0300

    done before finish step 2

--- logic_server.py
+++ logic_server.py
@@ -7,18 +7,17 @@ import yaml
 
 app = Flask(__name__)
 list_of_dict = [{
-    "fruit": "Apple",
-    "size": "Large",
-    "color": "Red"
 }]
 
-
+list_of_dict2 = [{
+}]
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 # step 2 - to receive JSON query from Front-End
+# step 3 send Yaml query from Logic to Query Server
 @app.route('/logic/query_data', methods=['POST'])
 def add():
     if request.method == 'POST':
@@ -26,8 +25,8 @@ def add():
         list_of_dict.append(data)
         data_yaml = yaml.dump(data) 		#JSON to YAML
         list_of_dict.append(data_yaml)
-        res = requests.post('http://localhost:5003/query/yml_data',data=data_yaml)
-    return data.status_code
+        res = requests.post('http://localhost:5003/query/yml_data',data=data_yaml,timeout=1.5)
+    return res.status_code
 
 # to check step 2 result
 @app.route('/logic/list', methods=['GET'])
@@ -36,14 +35,20 @@ def returnAll():
                            title='Home',
                            list_of_dict=list_of_dict)
 
-#step 3 send Yaml query from Logic to Query Server 
-#/query/yml_data
-#@app.route('/logic/intraday')
-#def test():
-#    json_file = open("intraday.json")
-#    intraday_query_json = json.load(json_file)
-#    res = requests.post('http://localhost:5002/logic/query_data',json=intraday_query_json)
-#    return res.status_cod
+# test step 4 Receiving Interday YAML data from Query server to Logic Server 
+#@app.route('/logic/query_data2', methods=['POST'])
+#def add():
+#    if request.method == 'POST':
+#        data2 = request.data
+#        list_of_dict2.append(data2)
+#    return 200
+
+#@app.route('/logic/list2', methods=['GET'])
+#def returnAll():
+#    return render_template("index.html",
+#                           title='Home',
+#                           list_of_dict2=list_of_dict2)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
--- query_emulation.py
+++ query_emulation.py
@@ -1,59 +1,44 @@
+#this is a Query server
+# it is Emulation of Intraday response
+
 from flask import Flask, request, jsonify, render_template
 import json
+import requests
+import yaml
 
 app = Flask(__name__)
 list_of_dict = [{
-    "fruit": "Apple",
-    "size": "Large",
-    "color": "Red"
 }]
+
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-
-@app.route('/api/add', methods=['POST'])
+# TEST to obtain YAML request from Logic server step 3
+# Send YAML reqest with intraday data to logic server  using data from https://intraday.worldtradingdata.com/api/v1/intraday?symbol=AAPL&range=1&interval=1&api_token=demo
+# local life "intraday_data.yml"
+@app.route('/query/yml_data', methods=['POST'])
 def add():
+    if request.method == 'POST':
+        data1 = request.data
+        list_of_dict.append(data1)
+    return data1.status_code
 
-    try:
-        if request.method == 'POST':
-            # data =request.json()
-            data = request.get_json()
-            print(request)
-        list_of_dict.append(data)
-        print(list_of_dict)
-    except:
-        print("This didn't work.")
-    return ""
-
-
-@app.route('/api/list', methods=['GET'])
+@app.route('/query/list', methods=['GET'])
 def returnAll():
     return render_template("index.html",
                            title='Home',
                            list_of_dict=list_of_dict)
 
+@app.route('/query/test')
+def test():
+    yaml_file = open("intraday_data.yml")
+    intraday_data_yaml = yaml.load(yaml_file)
+    res = requests.post('http://localhost:5002/logic/query_data2',data=intraday_data_yaml)
+    return res.status_code
 
-# @app.route('/')
-# @app.route('/index')
-# def returnAll1():
-#     # return jsonify(list_of_dict)
-#     # user = { 'nickname': 'Miguel' } 
-#     return render_template("index.html",
-#         title = 'Home',
-#         list_of_dict = list_of_dict)
-#     # return '''
-#     # <html>
-#     #   <head>
-#     #     <title>Home Page</title>
-#     #   </head>
-#     #   <body>
-#     #     <h1>Hello, ''' + list_of_dict + '''</h1>
-#     #   </body>
-#     # </html>
-#     # '''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003)
